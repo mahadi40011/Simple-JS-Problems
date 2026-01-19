19bb45425f31f496df0fc8fa2e8b32adfb9211ac
mahadi40011 <mahadi40011@gmail.com>
2026-01-19 10:59:06 +0600

    Problem 2 solved

--- index.js
+++ index.js
@@ -5,10 +5,18 @@ function reverseString(str) {
 console.log("Problem-1: ", reverseString("hello"));
 
 // Problem 2: Count Vowels in a String
-// Write a function that counts how many vowels (a, e, i, o, u) are in a given string.
-// Example:
-// Input: "programming"
-// Output: 3
+function countVowels(str) {
+  let count = 0;
+  const vowels = "aeiouAEIOU";
+
+  for (let char of str) {
+    if (vowels.includes(char)) {
+      count++;
+    }
+  }
+  return count;
+}
+console.log("Problem-2: ", countVowels("programming"));
 
 // Problem 3: Check for Palindrome
 // Write a function that checks if a string is a palindrome (reads the same forward and backward).
