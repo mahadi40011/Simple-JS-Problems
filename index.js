de15d544403e5c7dc879badacaec38f0b1e56cbc
mahadi40011 <mahadi40011@gmail.com>
2026-01-19 11:11:18 +0600

    Problem 10 solved

--- index.js
+++ index.js
@@ -72,11 +72,21 @@ function factorial(n) {
 console.log("Problem-9: ", factorial(5));
 
 // Problem 10: PingPong Challenge
-// Write a function that prints numbers from 1 to 20.
-// Example:
-// For multiples of 3, print "Ping"
-// For multiples of 5, print "Pong"
-// For multiples of both 3 and 5, print "PingPong"
-// If the number is not a multiple of 3 or 5, print the number itself
-// Example Output:
-// 1, 2, Ping, 4, Pong, Ping, 7, 8, Ping, Pong, 11, Ping, 13, 14, PingPong, 16 …..
+function pingPong() {
+  let result = "";
+
+  for (let i = 1; i <= 20; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      result += "PingPong ";
+    } else if (i % 3 === 0) {
+      result += "Ping ";
+    } else if (i % 5 === 0) {
+      result += "Pong ";
+    } else {
+      result += i + " ";
+    }
+  }
+
+  console.log("Problem-10: ", result.trim());
+}
+pingPong();
